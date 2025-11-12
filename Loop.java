47f6b7fb10c2a30f587bd6fdb47c563ffe8c542b
kanjitta <jittakan2539@gmail.com>
2025-11-12 22:40:48 +0700

    loop

---
+++ Loop.java
@@ -0,0 +1,9 @@
+class Loop {
+    public static void main(String[] args) {
+        //loop
+        for (int i = 0; i < 10; i++) {
+            System.out.println("It's less than 10.");
+        }
+        System.out.println("It's 10.");
+    }
+}
