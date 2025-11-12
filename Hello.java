3a460acf1becfb916310ccc346b4765631d64f06
kanjitta <jittakan2539@gmail.com>
2025-11-12 11:13:39 +0700

    add implicit variable declaation

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 class Hello {
+    
     public static void main(String[] args) {
         System.out.println("Hello World");
+        var age = 2;
+        System.out.println("I am " + age + " years old.");
     }
 }
