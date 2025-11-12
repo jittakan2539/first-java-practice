af7426b29d7d17aa044a9b9896c24f64870a3cf4
kanjitta <jittakan2539@gmail.com>
2025-11-12 11:24:21 +0700

    add more variables

--- Hello.java
+++ Hello.java
@@ -3,6 +3,10 @@ class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         var age = 2;
+        String country = "Thailand";
+        char nameInitial = 'M';
         System.out.println("I am " + age + " years old.");
+        System.out.println("I am from " + country + ".");
+        System.out.println("You can call me " + nameInitial + " for short.");
     }
 }
