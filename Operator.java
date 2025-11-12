f6b850431b090b74de6b76328d9dabea5a2f2309
kanjitta <jittakan2539@gmail.com>
2025-11-12 11:42:38 +0700

    forced casting with division

--- Operator.java
+++ Operator.java
@@ -11,7 +11,9 @@ class Operator {
         System.out.println(num1 / num3);
 
         //forced casting
-        float sum = (float) num1+ num2;
+        float sum = (float) num1 + num2;
+        float resultDivide = (float) num1 / (float) num2;
         System.out.println("This is " + sum + ".");
+        System.out.println("This is " + resultDivide + ".");
     }
 }
