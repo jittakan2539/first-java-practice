f9dccb26888019af518e51165fdd28da509aedb9
kanjitta <jittakan2539@gmail.com>
2025-11-12 11:35:49 +0700

    Add Operator

---
+++ Operator.java
@@ -0,0 +1,11 @@
+class Operator {
+    public static void main(String[] args) {
+        int num1 = 8;
+        int num2 = 5;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 % num2);
+    }
+}
