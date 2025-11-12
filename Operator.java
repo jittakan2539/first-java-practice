650b3c94d58df34ae12d531691d93021a9164aaa
kanjitta <jittakan2539@gmail.com>
2025-11-12 11:40:51 +0700

    added forced casting

--- Operator.java
+++ Operator.java
@@ -2,10 +2,16 @@ class Operator {
     public static void main(String[] args) {
         int num1 = 8;
         int num2 = 5;
+        float num3 = 6;
         System.out.println(num1 + num2);
         System.out.println(num1 - num2);
         System.out.println(num1 * num2);
         System.out.println(num1 / num2);
         System.out.println(num1 % num2);
+        System.out.println(num1 / num3);
+
+        //forced casting
+        float sum = (float) num1+ num2;
+        System.out.println("This is " + sum + ".");
     }
 }
